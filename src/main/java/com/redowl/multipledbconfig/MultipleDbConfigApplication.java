3234262744a7a39ffbd1766673dcecd955c1e7fc
gehan.chandima <gehanc@lk.onsystechnologies.com>
2024-05-10 22:23:40 +0530

    Initial Commit

---
+++ src/main/java/com/redowl/multipledbconfig/MultipleDbConfigApplication.java
@@ -0,0 +1,13 @@
+package com.redowl.multipledbconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDbConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDbConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redowl/multipledbconfig/MultipleDbConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redowl.multipledbconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDbConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
