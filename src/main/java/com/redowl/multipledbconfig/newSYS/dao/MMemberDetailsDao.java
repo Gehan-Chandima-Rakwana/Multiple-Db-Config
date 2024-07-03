1f5055bf2971cdd2473f2d54f94b7e48b2cf4f0a
gehan.chandima <gehanc@lk.onsystechnologies.com>
2024-07-03 15:05:39 +0530

    mod

--- src/main/java/com/redowl/multipledbconfig/newSYS/dao/MMemberDetailsDao.java
+++ src/main/java/com/redowl/multipledbconfig/newSYS/dao/MMemberDetailsDao.java
@@ -2,11 +2,12 @@ package com.redowl.multipledbconfig.newSYS.dao;
 
 import com.redowl.multipledbconfig.newSYS.models.MMemberDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MMemberDetailsDao extends JpaRepository<MMemberDetails, Integer> {
+public interface MMemberDetailsDao extends JpaRepository<MMemberDetails, Integer>, JpaSpecificationExecutor<MMemberDetails> {
     Optional<MMemberDetails> findByMemberIdAndIsActive(Long id, boolean b);
 }
